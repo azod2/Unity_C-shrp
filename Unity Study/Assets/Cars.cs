b6e18379ed729ceacf7888b59b385d1cb2c7f430
endless2code <a0750318@gmail.com>
2020-04-08 15:32:59 +0800

    認識欄位

--- Unity Study/Assets/Cars.cs
+++ Unity Study/Assets/Cars.cs
@@ -1,29 +1,70 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 //註解:用來說明紀錄等等.. 只會在此腳本內看到
 //摘要 summary : 描述此內容，會在其他腳本上看到-提高維護性與擴充性
 /// <summary>
 /// 定義汽車物件
 /// </summary>
 
+//新增的C#放在專案內為藍圖
+//放在場景物件上為實體物件
+
+
 public class Cars : MonoBehaviour
 {
     //認識第一個成員 : 欄位 field - 儲存物件資料
-    //資料類型
+    //資料類型:
     //CC數 - 2000(整數-int)
     //重量 - 100.9(浮點數-float)
     //品牌 - BMW(字串-string)
     //煞車 - 是否開啟(布林值-bool)
 
     //定義欄位語法:
-    //資料類型 欄位名稱;
-    //預設值
+    //修飾詞 資料類型 欄位名稱(指定 值);
+
+    //修飾詞 :
+    //公開 public : 允許其他類別存取，顯示在屬性面板(Inspector)上
+    //私人 private (預設) : 不允許、不顯示
+
+    //預設值 :
     //int、float為0
     //string為""
     //bool為false
 
-    int cc = 2000;
-    float weight = 100.9f;
-    string brand = "BMW";
-    bool brake =false;
+    //欄位屬性 Attribute : Unity 提供用於輔助欄位的功能
+    //語法:[屬性名稱(值)]
+    //標題 [Header("字串")]
+    //提示[Tooltip("字串")]
+    //範圍[Range(最小值,最大值)] - 僅限於數值類型資料int、float
 
+
+    /// <summary>
+    /// 汽車的CC數
+    /// </summary>
+    [Header("汽車的CC數"),Range(200,2000)]
+    public int cc = 2000;
+    /// <summary>
+    /// 汽車的重量
+    /// </summary>
+    [Tooltip("這個欄位是用來存放汽車的重量")]
+    public float weight = 100.9f;
+    /// <summary>
+    /// 汽車的品牌
+    /// </summary>
+    [Header("品牌")]
+    [Tooltip("代表這台車子的品牌")]
+    public string brand = "BMW";
+    /// <summary>
+    /// 汽車剎車
+    /// </summary>
+    [Header("是否剎車"),Tooltip("用來儲存這台車子目前的剎車狀態")]
+    public  bool brake =false;
+
+
+    
+    public void Start()
+    {
+        transform.Translate(1, 10, 10);
+
+    }
 }
--- Unity Study/Assets/Class1.cs
+++ Unity Study/Assets/Class1.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;  //引用Unity API (函式庫 : Unity 屬性、功能)
+
+//類別 class 定義一個物件
+//語法 : class類別名稱
+// : MonoBehavior可以將此腳本掛在Unity物件上
+//類別必須放在物件上才會執行
 
 public class Class1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //定義類別成員
+    //C#
+    //1. 大小寫有區分
+    //2. 括號成對出現
+
+    [Header("汽車一號")]
+    public Cars car1;
+    [Header("汽車二號")]
+    public Cars car2;
+
+    //事件 : 在特定時間會依指定次數執行的方法(名稱為藍色)
+    //開始事件 : 撥放遊戲時執行一次 - 初始事件
+
     void Start()
     {
-        
+        print("Hello world");
+
+        //取得 get
+        print("汽車一號的CC數:"+car1.cc);
+        print("汽車二號的CC數:"+car2.cc);
+        //設定set
+        car1.cc = 500;
+        car1.brand = "TOYOTA";
+        car1.weight = 500.64f;
+        car1.brake = true;
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
